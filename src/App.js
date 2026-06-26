9856cbcb0db19da8430eb6fa49a941a51ff87681
priyadharshini000 <pd3611300@gmail.com>
2026-06-26 12:22:50 +0530

    Fix mobile layout css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import WhatsAppIcon from './components/WhatsAppIcon';
+
 import Home from './pages/Home';
+import Migrate from './pages/Migrate';
 import WorkVisa from './pages/WorkVisa';
 import StudyVisa from './pages/StudyVisa';
+import Visitvisa from './pages/Visitvisa';
+import DependentFamily from './pages/Dependent-Family';
 import Contact from './pages/Contact';
 
 export default function App() {
@@ -14,23 +18,41 @@ export default function App() {
     switch (currentPage) {
       case 'home':
         return <Home setCurrentPage={setCurrentPage} />;
+
+      case 'migrate':
+        return <Migrate />;
+
       case 'work':
         return <WorkVisa />;
+
       case 'study':
         return <StudyVisa />;
+
+      case 'visit':
+        return <Visitvisa />;
+
+      case 'dependent':
+        return <DependentFamily />;
+
       case 'contact':
         return <Contact />;
+
       default:
         return <Home setCurrentPage={setCurrentPage} />;
     }
   };
 
   return (
-    <div className="flex flex-col min-h-screen font-sans antialiased text-gray-900 selection:bg-amber-200">
-      <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <main className="flex-grow">
+    <div className="w-full min-h-screen overflow-x-hidden flex flex-col font-sans antialiased text-gray-900 selection:bg-amber-200">
+      <Navbar
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+
+      <main className="flex-grow w-full">
         {renderPage()}
       </main>
+
       <Footer setCurrentPage={setCurrentPage} />
       <WhatsAppIcon />
     </div>
